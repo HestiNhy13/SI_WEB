708d22ad26bad93e1d8359c36ae3928c5ebc09e4
HestiNhy13 <158534075+HestiNhy13@users.noreply.github.com>
2025-02-13 21:23:50 +0700

    Update web.php
    komen

--- hesti_project/routes/web.php
+++ hesti_project/routes/web.php
@@ -1,4 +1,7 @@
 <?php
+
+// --------------------ACARA 3--------------------
+
 // mengimport kelas route dari laravel
 use Illuminate\Support\Facades\Route;
 // route untuk halaman utama
@@ -26,36 +29,46 @@ Route::get('/user','UserController@index');
 
 // regedit otomatis 
 Route::redirect('/coba','/sini');
-// dengan tampilan view dan data yang diisi
+// route untuk halaman utama ('/')
 Route::get('/', function(){
-    return view('profile',[
+    return view('profile',[ //mengembalikan view dengan data nama dan nim
         'nama' => 'Hesti',
         'nim'=> 'e41232454'
     ]);
 });
+// route dengan parameter optional
 Route::get('user1/{name?}', function ($name = null){
-    return $name? "Hallo, $name!" : "Hallo";
+    return $name? "Hallo, $name!" : "Hallo";//menampilkan "hallo" 
 });
+//route dengan parameter opsional,ada nilai default'hesti'
 Route::get('user2/{name?}', function ($name = 'Hesti'){
     return $name? "Hallo, $name!" : "Hallo Hesti";
 });
+//route dgn parameter wajib 'name', harus huruf
 Route::get('user3/{name}', function($name){
     return "selamat, $name!";
 })->where('nama', '[A-Za-z]+');
+//route dgn parameter 'id' berupa angka
 Route::get('user4/{id}', function ($id){
     return "User ID: $id";
 })->where('id', '[0-9]+');
+//route dengan parameter 'name' harus huruf kecil, 'id' harus angka
 Route::get('user5/{id}/{name}', function ($id, $name){
     return "User ID: $id, Name: $name";
 })->where(['id'=>'[0-9]+','name' => '[a-z]+']);
+//route dgn parameter 'search' nisa menerima semua jenis karakter
 Route::get('search/{search}', function($search){
     return $search;
-})->where('search', '.*');
+})->where('search', '.*');//menerima semua yang diinputkan
 
-use App\Http\Controllers\UserProfileController;
+
+
+// --------------------ACARA 4--------------------
+use App\Http\Controllers\ProfileController;
 
 Route::get('user6/profile', function(){
     return "ini adalah halaman user6!";
 })->name('profile.user6');
 
-Route::get('user7/profile', [UserProfileController::class, 'show'])->name('profile.user6');
+Route::get('user7/profile', ['ProfileController@show'])->name('profile.user6');
+

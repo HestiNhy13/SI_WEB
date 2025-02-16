747d8ff3ac19abc580baa18ced454128f7aa130b
HestiNhy13 <158534075+HestiNhy13@users.noreply.github.com>
2025-02-16 19:56:45 +0700

    Update ManagementUserController.php
    controller

--- hesti_project/app/Http/Controllers/ManagementUserController.php
+++ hesti_project/app/Http/Controllers/ManagementUserController.php
@@ -4,6 +4,27 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 class ManagementUserController extends Controller{
     public function index(){
-        return "ini adalah method index";
+        $name = "Hesti Nurhayati";
+        $nim = "E41232454";
+        return view('welcome', compact('name', "nim"));
     }
+    public function create(){
+        return "menampilkan halaman form tambah data";
+    }
+    public function store(Request $request){
+        return "untuk menciptakan data admin yang baru";
+    }
+    public function show(){
+        return "adalah halaman show";
+    }
+    public function edit($id){
+        return "menampilkan form untuk mengubah dan edit dengan id = ".$id;
+    }
+    public function update(Request $request, $id){
+        return"digunakan untuk mengubah data admin dengan id = ";
+    }
+    public function destroy($id){
+        return "digunakan untuk menghapus data admin dengan id =". $id;
+    }
+    
 }

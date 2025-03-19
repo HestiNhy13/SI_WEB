b0db2a850bb6d16844e98017421d95ada4e0723b
HestiNhy13 <158534075+HestiNhy13@users.noreply.github.com>
2025-03-19 21:21:49 +0700

    21-22

--- hesti_project/app/Providers/AppServiceProvider.php
+++ hesti_project/app/Providers/AppServiceProvider.php
@@ -1,24 +1,21 @@
 <?php
 
-namespace App\Providers;
-
+ use Intervention\Image\ImageManager;
+ use Intervention\Image\Drivers\Gd\Driver;
+ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        Route::middleware('api')
+     ->prefix('api')
+     ->group(base_path('routes/api.php'));
     }
 }
--- hesti_project/routes/api.php
+++ hesti_project/routes/api.php
@@ -1,19 +1,16 @@
 <?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+ use Illuminate\Http\Request;
+ use Illuminate\Support\Facades\Route;
+ use App\Http\Controllers\Backend\ApiPendidikanController;
+ 
+ // Route default Laravel untuk user
+ Route::middleware('auth:api')->get('/user', function(Request $request){
+     return $request->user();
+ });
+ 
+ // API Pendidikan
+ Route::get('api_pendidikan', [ApiPendidikanController::class, 'getAll']);
+ Route::get('api_pendidikan/{id}', [ApiPendidikanController::class, 'getPen']);
+ Route::post('api_pendidikan', [ApiPendidikanController::class, 'createPen']);
+ Route::put('api_pendidikan/{id}', [ApiPendidikanController::class, 'updatePen']);
+ Route::delete('api_pendidikan/{id}', [ApiPendidikanController::class, 'deletePen']);
